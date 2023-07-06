982bdadfe6fbc8d976483a7da1e02ef38bf8a045
MarcelFeo <marcelfeo29@gmail.com>
2023-07-06 17:03:42 -0300

    feat: add getUsers

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,17 +1,29 @@
 class Api::V1::UsersController < ApplicationController
+  #get
   def getUsers
-
+    user = User.all
+    if user
+      render json: user, status: :ok
+    else
+      render json: { msg: "user empty" }, status: :unprocessable_entity
+    end
   end
 
+  # post
   def addUser
-    user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    user = User.new(userparams)
 
     if user.save()
       render json: user, status: :ok
     else
-      render json: {message: "User not added"}, status: :unprocessable_entity
+      render json: { msg: "User not added" }, status: :unprocessable_entity
     end
 
+  end
+
+  private
+  def userparams
+    params.permit(:username, :email, :password_digest)
   end
 
 end
